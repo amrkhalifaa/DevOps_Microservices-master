bb333697d29ab81a85b6a63f59ab6180c6c200a7
amrkhalifaa <you@example.com git config --global user.email amrkhalifaa95@gmail.com>
2022-10-22 00:46:41 +0200

    ddknksd

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
@@ -6,10 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="fahmykhalilm/ml_project"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml-app:latest $dockerpath:ml-app
+cat .my_password.txt | docker login --username fahmykhalilm --password-stdin
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:ml-app
+
 
 # Step 3:
 # Push image to a docker repository
